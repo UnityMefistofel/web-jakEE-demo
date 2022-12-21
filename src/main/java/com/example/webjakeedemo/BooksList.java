e41a1d9cafb4ff1dd0f9a588fa27a907946b43e6
UnityMefistofel <mefistofel.sf@mail.ru>
2022-12-21 23:59:42 +0300

    Added DBQuery servlets

--- src/main/java/com/example/webjakeedemo/BooksList.java
+++ src/main/java/com/example/webjakeedemo/BooksList.java
@@ -1,18 +1,14 @@
 package com.example.webjakeedemo;
 
-import com.example.webjakeedemo.Util;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.SneakyThrows;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @WebServlet(name = "bookslist", value = "/bookslist")
